f88150190d9a4341148a29406a94d8f55877d7e8
amit <amitdhanik3@gmail.com>
2018-06-30 13:26:45 +0530

    added textfields from database that show on  websites

--- jobs/views.py
+++ jobs/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
+from .models import Job #job object so we can access all jobs
 
-# Create your views here.
+def home(request):
+    jobs= Job.objects #brings all job object from database and turn them into python objects which can be used inside of our code.
+    return render(request,'jobs/home.html', {'jobs': jobs })
--- portfolio/urls.py
+++ portfolio/urls.py
@@ -17,7 +17,9 @@ from django.contrib import admin
 from django.urls import path
 from django.conf import settings #importing settings.py
 from django.conf.urls.static import static#import staticfiles to upload images,videos
+import jobs.views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('',jobs.views.home,name='home'),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
